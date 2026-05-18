823f277170f725b19b1579fe782115322a189121
Cr1stopher636 <ricardo0640@outlook.com>
2026-05-18 09:40:22 -0600

    change: vector

--- vector.cpp
+++ vector.cpp
@@ -5,15 +5,33 @@
 using namespace std;
 
 int main() {
-    vector<int> original = {1,2,3,4,5};
 
-    vector<int> nuevoVector = std::move(original);
+    vector<string> original = {"1,2,3,4,5", "ricardo"};
+    vector<string> nuevoVector = std::move(original);
 
     cout << "el tamaño del original es: " << original.size() << endl;
     cout << "el tamaño del nuevo es: " << nuevoVector.size() << endl;
 
     return 0;
 }
+
+// int main() {
+//     vector<string> nombres(5);
+
+//     nombres[0] = "ricardo";
+//     nombres[1] = "mercedes";
+//     nombres[2] = "iphone";
+//     nombres[3] = "bmw";
+//     nombres[4] = "cadillac";
+
+//     cout << nombres[0] << endl;
+//     cout << nombres[1] << endl;
+//     cout << nombres[2] << endl;
+//     cout << nombres[3] << endl;
+//     cout << nombres[4] << endl;
+
+//     return 0;
+// }
 
 
 

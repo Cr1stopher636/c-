d2c080a77c7c881bb49237a685f17f4d2c0ecdb3
Cr1stopher636 <ricardo0640@outlook.com>
2026-05-18 10:04:40 -0600

    add: pair file

---
+++ pair.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <utility>
+
+using namespace std;
+
+int main() {
+    pair <string, int> persona("ricardo", 23);
+
+    cout << "nombre: " << persona.first << endl;
+    cout << "edad: " << persona.second << endl;
+
+    return 0;
+}

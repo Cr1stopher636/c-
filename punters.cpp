d92ea1f3b7012111a5375e40a3ec792766bd4596
Cr1stopher636 <ricardo0640@outlook.com>
2026-05-18 07:26:12 -0600

    terminado el ejercicio de hilos y punteros y ciclo while

--- punters.cpp
+++ punters.cpp
@@ -5,17 +5,27 @@
 using namespace std;
 
 
-int main(int argc, char *argv[0]) {
+// int main(int argc, char *argv[0]) {
 
+//     int numero = 10;
+//     int *mipuntero; // un puntero guarda direcciones de memoria
+
+//     mipuntero = &numero; // & indica la direccion en memoria de la variable
+//     cout << *mipuntero << endl; // imprime el valor usando el puntero
+//     cout << mipuntero << endl; // imprime la direccion en memoria de la variable 
+
+// }
+
+
+int main() {
+    
     int numero = 10;
-    int *mipuntero; // un puntero guarda direcciones de memoria
+    int *puntero;
 
-    mipuntero = &numero; // & indica la direccion en memoria de la variable
-    cout << *mipuntero << endl; // imprime el valor usando el puntero
-    cout << mipuntero << endl; // imprime la direccion en memoria de la variable 
+    puntero = &numero;
 
- 
-
+    cout << puntero << endl;
+    cout << *puntero << endl;
 }
 
 
--- threads#2.cpp
+++ threads#2.cpp
@@ -4,22 +4,27 @@
 
 using namespace std;
 
-void primerfunction() {
-    cout << "prmer mensaje" << endl;
+void primerafunction() {
+    cout << "primer mensaje" << endl;
 }
 
 
-void segundafunction(string nombre, thread primerhilo) {
+void segundafunction(string nombre, thread&& primerhilo) {
+    cout << "el nombre es: " << nombre << endl;
+
     if (primerhilo.joinable()) {
         primerhilo.join();
     }
+    
 }
 
-int main() {
-    thread t1(primerfunction);
-    string nombre = "ricardo";
 
-    thread t2(segundafunction, nombre, std::move(t1));
+int main() {
+
+    thread t1(primerafunction);
+    string minombre = "ricardo";
+
+    thread t2(segundafunction, minombre, std::move(t1));
     t2.join();
 
     return 0;

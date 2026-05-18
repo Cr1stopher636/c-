5b5d41610f920c8b0a3efb5cc71f31e58f581013
Cr1stopher636 <ricardo0640@outlook.com>
2026-05-18 07:43:00 -0600

    finish antro

---
+++ antro.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+    int edad = 23;
+    if (edad >= 18) {
+        cout << "Bienvenido puedes ingresar" << endl;
+    } else {
+        cout << "Lo sentimos no puedes ingresar" << endl;
+        
+    }
+    return 0;
+
+}
+
+

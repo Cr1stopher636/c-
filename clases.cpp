53533528cac029f56acff3e31dac8be58b573cad
Cr1stopher636 <ricardo0640@outlook.com>
2026-05-19 11:44:00 -0600

    add: clases

---
+++ clases.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Persona {
+    public:
+
+    string nombre;
+    int edad;
+
+
+
+    Persona(string _nombre, int edad) {
+        nombre = nombre;
+        edad = edad;
+    }
+
+
+    void saludar() {
+        cout << "Hola, mi nombre es " << nombre << " y tengo " << edad << "años." << endl;
+    }
+}; // <---------- Punto y coma en la clase
+
+
+// uso de la clase en el programa principal
+int main() {
+    Persona usuario1("Carlos", 23);
+
+
+    // Accedemos a sus metodos usando el operador (.)
+    usuario1.saludar();
+
+    return 0;
+}
+
+
+
+

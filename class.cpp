0420e8d1290d7ffc5a9dcf4dd7989126bebd121e
Cr1stopher636 <ricardo0640@outlook.com>
2026-05-19 16:50:25 -0600

    fixed: modified class

---
+++ class.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Persona {
+    public:
+
+    string nombre;
+    int edad;
+
+
+    Persona() { // Constructor
+        nombre = "ricardo";
+        edad = 23;
+
+    }
+};
+
+
+int main() {
+
+    Persona persona1;
+
+    cout << persona1.nombre << endl;
+    cout << persona1.edad << endl;
+    
+
+
+    
+   
+}

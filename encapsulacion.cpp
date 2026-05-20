c2786e7c93ff21ab5aa1eca3ac461ec611927d4c
Cr1stopher636 <ricardo0640@outlook.com>
2026-05-20 16:45:16 -0600

    change: Objects

--- encapsulacion.cpp
+++ encapsulacion.cpp
@@ -38,13 +38,22 @@ class Animal {
 
 int main() {
 
-    Animal a1;
+    Animal a1; // <-- objeto a1
+    Animal a2; // <-- objeto a2
 
     a1.setNombre("Dragon de komodo");
     a1.setEspecie("Reptil");
 
-    cout << "El nombre del animal es: " << a1.getNombre() << endl;
-    cout << "La especie es: " << a1.getEspecie() << endl;
+    a2.setNombre("Pinguino");
+    a2.setEspecie("Ave acuatica");
+
+    cout << "El nombre del animal del primer objeto es: " << a1.getNombre() << endl;
+    cout << "La especie del primer objeto es: " << a1.getEspecie() << endl;
+
+    cout << "\n" << endl;
+
+    cout << "El nombre del animal del segundo objeto es: " << a2.getNombre() << endl;
+    cout << "La especie del segundo objeto es: " << a2.getEspecie() << endl;
 
     return 0;
 }

0387eb4ac59deed937b10fedf13d0a33d39446d2
Cr1stopher636 <ricardo0640@outlook.com>
2026-05-21 16:55:24 -0600

    add: This

--- clases.cpp
+++ clases.cpp
@@ -12,14 +12,20 @@ class Persona {
 
 
 
+    // Persona() {
+    //     nombre = "";
+    //     edad = 0;
+    // }
+
+
     Persona(string nombre, int edad) {
-        nombre = nombre;
-        edad = edad;
+        this-> nombre = nombre;
+        this-> edad = edad;
     }
 
 
     void saludar() {
-        cout << "Hola, mi nombre es " << nombre << " y tengo " << edad << "años." << endl;
+        cout << "Hola, mi nombre es " << nombre << " y tengo " << edad << " años." << endl;
     }
 }; // <---------- Punto y coma en la clase
 
@@ -27,11 +33,20 @@ class Persona {
 
 // uso de la clase en el programa principal
 int main() {
+
+   
     Persona usuario1("Carlos", 23);
+    Persona usuario2("juan", 20);
+    
 
 
     // Accedemos a sus metodos usando el operador (.)
     usuario1.saludar();
+    usuario2.saludar();
+
+ 
+
+    
 
     return 0;
 }

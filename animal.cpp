0842530fbad7d10073d6e78116be5d2bdad88940
Cr1stopher636 <ricardo0640@outlook.com>
2026-05-21 18:18:00 -0600

    Add: Animal class

---
+++ animal.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Animal {
+    public:
+
+    string nombre;
+    int edad;
+
+  
+    //Metodos ------ Cuando una funcion esta dentro de una clase (Animal) <-- es la clase
+    void caminar() {
+        cout << "El animal " << nombre << "esta caminando" << endl;
+    }
+
+    void comer() {
+        cout << "El animal " << nombre << "esta comiendo" << endl;
+    }
+
+
+    Animal(string nombre, int edad) {
+        cout << "El animal es un " << nombre << " y tiene " << edad << " años de edad" << endl;
+    }
+
+};
+
+
+int main() {
+    Animal a1("cocodrilo", 5);
+
+    a1.comer();
+    a1.caminar();
+}

44343440d8c921a83b6cd97f464ffce5f95848ce
Cr1stopher636 <ricardo0640@outlook.com>
2026-05-28 12:31:49 -0600

    change: cambios en el puntero

--- puntero3.cpp
+++ puntero3.cpp
@@ -2,17 +2,18 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main() {
     int numero = 10;
     int *miPuntero;
 
     miPuntero = &numero;
 
 
+
     cout << "Esta es la direccion en memoria: " << miPuntero << endl;
     cout << "Este es el valor en memoria: " << *miPuntero << endl;
 
-    getchar();
+    // getchar();
 
     return 0;
 }
